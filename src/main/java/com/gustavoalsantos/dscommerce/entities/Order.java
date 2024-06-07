6232309eb94132c5d73cd8c4a05a48122313b109
Gustavo Almeida <gustavoalsantos99@gmail.com>
2024-06-07 00:02:45 -0300

    Payment

--- src/main/java/com/gustavoalsantos/dscommerce/entities/Order.java
+++ src/main/java/com/gustavoalsantos/dscommerce/entities/Order.java
@@ -16,18 +16,25 @@ public class Order {
     private Instant moment;
     private OrderStatus status;
 
+    //Relacionamento muitos para um
     @ManyToOne
     @JoinColumn(name = "client_id")
-    private User client; //Relacionamento muitos para um
+    private User client;
+
+    //Relacionamento um para um - independente
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
         this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -60,5 +67,13 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }

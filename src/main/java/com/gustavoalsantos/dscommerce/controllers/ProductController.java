b0b7635ab1297c7d31c68ca82a1a83f50a6c63ee
Gustavo Almeida <gustavoalsantos99@gmail.com>
2024-06-19 16:24:28 -0300

    ValidationError

--- src/main/java/com/gustavoalsantos/dscommerce/controllers/ProductController.java
+++ src/main/java/com/gustavoalsantos/dscommerce/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.gustavoalsantos.dscommerce.dto.ProductDTO;
 import com.gustavoalsantos.dscommerce.entities.Product;
 import com.gustavoalsantos.dscommerce.repositories.ProductRepository;
 import com.gustavoalsantos.dscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,14 +35,14 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }

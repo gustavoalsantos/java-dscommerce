3a5baab6d5f59cc1e14c94e33710d3c57d7ac668
Gustavo Almeida Santos <gustavoalsantos99@gmail.com>
2024-07-07 22:58:12 -0300

    Added Access Control

--- src/main/java/com/gustavoalsantos/dscommerce/entities/Role.java
+++ src/main/java/com/gustavoalsantos/dscommerce/entities/Role.java
@@ -4,6 +4,8 @@ package com.gustavoalsantos.dscommerce.entities;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_role")
 public class Role implements GrantedAuthority {
@@ -35,5 +37,18 @@ public class Role implements GrantedAuthority {
 
     public void setAuthority(String authority) {
         this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(authority);
     }
 }
